c21fe713f47909667492b23c3238eeb1705271dd
ysgo <wjion8428@gmail.com>
2021-02-03 23:38:47 +0900

    카테고리 기능 개선과 데이터 스토어 확장 및 상품 카트 기능 추가

--- src/shop/CategoryNavigation.js
+++ src/shop/CategoryNavigation.js
@@ -1,15 +1,16 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
   render() {
     return <React.Fragment>
-      <Link to={this.props.baseUrl} className="btn btn-secondary btn-block">All</Link>
+      <ToggleLink to={this.props.baseUrl} className="btn btn-secondary btn-block">All</ToggleLink>
       { this.props.categories && this.props.categories.map(cat =>
-        <Link className="btn btn-secondary btn-block"
+        <ToggleLink className="btn btn-secondary btn-block"
               key={cat} to={`${this.props.baseUrl}/${cat.toLowerCase()}`}>
           {cat}
-        </Link>
+        </ToggleLink>
       )}
     </React.Fragment>
   }
